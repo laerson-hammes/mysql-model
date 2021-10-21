d1b07bc7d8301d115f72c4944dee59a9ba50b8aa
laerson-hammes <laersonha@gmail.com>
2021-10-21 19:51:23 -0300

    Add Flake8

--- model/connection.py
+++ model/connection.py
@@ -4,29 +4,27 @@ from typing import Any, Dict, Union
 
 
 class Connection(object):
+    
+    HOST: str = 'localhost'
+    USER: str = 'root'
+    PORT: Union[int, None] = None
+    PASSWORD: str = ''
+    AUTOCOMMIT: bool = True
+    CURSORCLASS: type = pymysql.cursors.DictCursor
+    
+    def __init__(self, **kwargs: Dict[str, Any]) -> None:
+        self.__kwargs: Dict[str, Any] = kwargs
+        self.connect()
    
-   HOST: str = 'localhost'
-   USER: str = 'root'
-   PORT: Union[int, None] = None
-   PASSWORD: str = ''
-   AUTOCOMMIT: bool = True
-   CURSORCLASS: type = pymysql.cursors.DictCursor
+    def execute(self, query: str, /) -> None:
+        connection = self.connect()
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                return cursor.fetchall()
    
-   def __init__(self, **kwargs: Dict[str, Any]) -> None:
-      self.__kwargs: Dict[str, Any] = kwargs
-      self.connect()
-   
-   
-   def execute(self, query: str, /) -> None:
-      connection = self.connect()
-      with connection: 
-         with connection.cursor() as cursor:
-            cursor.execute(query)
-            return cursor.fetchall()
-   
-   
-   def connect(self, /):
-      try:
-         return pymysql.connect(**self.__kwargs)
-      except OperationalError:
-         raise OperationalError()
+    def connect(self, /):
+        try:
+            return pymysql.connect(**self.__kwargs)
+        except OperationalError:
+            raise OperationalError()
