09821544c00be8ad0939e48bc7a4a83f38dfefcc
laerson-hammes <laersonha@gmail.com>
2021-10-22 05:31:13 -0300

    Add a new class with the connection atributes and an update_atribute function

--- model/connection.py
+++ model/connection.py
@@ -1,30 +1,41 @@
 import pymysql.cursors
 from pymysql.err import OperationalError
-from typing import Any, Dict, Union
+from typing import Any, Dict
 
 
-class Connection(object):
+class ConnectionAttributes(object):
     
-    HOST: str = 'localhost'
-    USER: str = 'root'
-    PORT: Union[int, None] = None
-    PASSWORD: str = ''
-    AUTOCOMMIT: bool = True
-    CURSORCLASS: type = pymysql.cursors.DictCursor
-    
-    def __init__(self, **kwargs: Dict[str, Any]) -> None:
-        self.__kwargs: Dict[str, Any] = kwargs
-        self.connect()
-   
-    def execute(self, query: str, /) -> None:
+    ATTRS: Dict[str, Any] = {
+        'host': 'localhost',
+        'user': 'root',
+        'port': None,
+        'password': '',
+        'autocommit': True,
+        'cursorclass': pymysql.cursors.DictCursor
+    }
+
+
+    def update_atribute(self, **kwargs: Dict[str, Any]) -> None:
+        self.ATTRS.update(kwargs)
+        print(self.ATTRS)
+
+
+class Connection(ConnectionAttributes):
+
+    def __init__(self, /) -> None:
+        super().__init__()
+
+
+    def execute(self, query: str, /) -> Any:
         connection = self.connect()
         with connection:
             with connection.cursor() as cursor:
                 cursor.execute(query)
-                return cursor.fetchall()
-   
+                return cursor.fetchall() or cursor.rowcount or cursor.fetchone()
+
+
     def connect(self, /):
         try:
-            return pymysql.connect(**self.__kwargs)
+            return pymysql.connect(**self.ATTRS)
         except OperationalError:
             raise OperationalError()
