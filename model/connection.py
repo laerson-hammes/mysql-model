845114572c53b992d041bdf18933baddfd002c14
laerson-hammes <laersonha@gmail.com>
2021-10-27 00:22:29 -0300

    Extracting the execute function into a class

--- model/connection.py
+++ model/connection.py
@@ -4,7 +4,7 @@ from typing import Any, Dict
 
 
 class ConnectionAttributes(object):
-    
+
     ATTRS: Dict[str, Any] = {
         'host': 'localhost',
         'user': 'root',
@@ -17,21 +17,12 @@ class ConnectionAttributes(object):
 
     def update_atribute(self, **kwargs: Dict[str, Any]) -> None:
         self.ATTRS.update(kwargs)
-        print(self.ATTRS)
 
 
 class Connection(ConnectionAttributes):
 
     def __init__(self, /) -> None:
-        super().__init__()
-
-
-    def execute(self, query: str, /) -> Any:
-        connection = self.connect()
-        with connection:
-            with connection.cursor() as cursor:
-                cursor.execute(query)
-                return cursor.fetchall() or cursor.rowcount or cursor.fetchone()
+        super(ConnectionAttributes, self).__init__()
 
 
     def connect(self, /):
