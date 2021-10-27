caf856a316a8f147ebd7aeb065065385c9288632
laerson-hammes <laersonha@gmail.com>
2021-10-27 00:48:47 -0300

    Adding __init__ file

---
+++ __init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.0.1"
+
+
+from model.connection import Connection
+from model.connection import ConnectionAttributes
+from model.execute import Execute
+from model.database import Database
+from model.mysql import MySQL
+from model.table import Table
