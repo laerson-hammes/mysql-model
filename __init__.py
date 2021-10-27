3243421267b2bf6cddd3426897e7e0084f221916
laerson-hammes <laersonha@gmail.com>
2021-10-27 01:27:16 -0300

    Add .vscode to .gitignore and commenting imports of __init__ from home

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 __version__ = "0.0.1"
 
 
-from database.connection import Connection
-from database.connection import ConnectionAttributes
-from database.execute import Execute
-from database.database import Database
-from database.table import Table
+# from database.connection import Connection
+# from database.connection import ConnectionAttributes
+# from database.execute import Execute
+# from database.database import Database
+# from database.table import Table
 
-from mysql.mysql import MySQL
+# from mysql.mysql import MySQL
