b9a7810bf3c836cdacb6b523c69fcead1854bce5
laerson-hammes <laersonha@gmail.com>
2021-10-27 00:29:59 -0300

    Add drop and describe functions

--- model/table.py
+++ model/table.py
@@ -1,15 +1,22 @@
-from .connection import Connection
+from .execute import Execute
+# from ..fields.int import IntTypes
+# from ..fields.fields import Fields
 
 
-class Table(Connection):
+class Table(Execute):
+
     def __init__(self, table: str, /) -> None:
         super().__init__()
         self.table = table
-        
-    
+
+
     def create(self, /) -> bool:
         pass
-    
-    
+
+
     def drop(self, /) -> bool:
-        pass
+        return self.execute(f"DROP TABLE {self.table};")
+
+
+    def describe(self, /):
+        return self.execute(f"DESCRIBE TABLE {self.table};")
